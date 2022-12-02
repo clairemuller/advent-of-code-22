3d36a50951751a18127edab850914f4e8123d5b2
Claire Muller <claire@reamaze.com>
2022-12-01 18:14:14 -0800

    day 1 pt 1

--- 01.php
+++ 01.php
@@ -6,10 +6,22 @@ $arr = explode("\n", $str);
 // will look like:
 // [ [0]=> "1000" [1]=> "2000" [2]=> "3000" [3]=> "" [4]=> "4000" [5]=> "" [6]=> "5000" [7]=> "6000" [8]=> "" [9]=> "7000" [10]=> "8000" [11]=> "9000" [12]=> "" [13]=> "10000" [14]=> "" ]
 
-var_dump($arr);
+$mostCalories = 0;
+$currentCalCount = 0;
 
-// foreach ($arr as $value) {
-//   echo $value;
-// }
+foreach ($arr as $value) {
+  // add each $value to $currentCalCount until $value is an empty string
+  // then see if $currentCalCount is greater than $mostCalories
+  // if so, update $mostCalories
+  if (strlen($value) == 0) {
+    if ($currentCalCount > $mostCalories) {
+      $mostCalories = $currentCalCount;
+    }
+    $currentCalCount = 0;
+  } else {
+    $currentCalCount += $value;
+  }
+}
 
-// echo $str;
+// answer is:
+echo $mostCalories;
